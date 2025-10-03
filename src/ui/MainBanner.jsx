d45ff366e273a3e316a6c3a76d87bee5c34b0509
AliMahmoudi <alimahmoudi21a@gmail.com>
2025-10-03 13:07:18 +0330

    Change MainBanner For Better Responsive

--- src/ui/MainBanner.jsx
+++ src/ui/MainBanner.jsx
@@ -5,16 +5,16 @@ import OffLable from "./OffLable";
 
 const MainBanner = () => {
   return (
-    <section className="h-191 bg-mainBannerBG pl-91 pr-60 relative">
-      <div className="flex items-center justify-between h-full">
-        <div className="flex flex-col justify-center">
+    <section className="bg-mainBannerBG relative">
+      <div className="container flex items-center justify-center pt-25 pb-9 flex-wrap-reverse gap-6 xl:justify-between">
+        <div className="flex flex-col justify-center text-center xl:text-left">
           <h5 className="text-headerPink font-bold ">
             Best Furniture For Your Castle....
           </h5>
-          <h1 className="font-josefin font-bold text-[53px] my-3 max-w-161">
+          <h1 className="font-josefin font-bold text-[40px] my-3 max-w-131">
             New Furniture Collection Trends in 2020
           </h1>
-          <p className="font-bold text-mainBannerSubText mb-7 max-w-140">
+          <p className="font-bold text-mainBannerSubText mb-7 max-w-128">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in
             est adipiscing in phasellus non in justo.
           </p>
@@ -28,15 +28,15 @@ const MainBanner = () => {
             alt="Wooden armchair with cushions"
             className="z-10"
           />
-          <div className="size-162 bg-mainBannerCircles/35 rounded-full absolute bottom-2 z-4"></div>
-          <div className="size-162 bg-mainBannerCircles/35 rounded-full absolute bottom-4 left-14 z-4"></div>
+          <div className="size-152 bg-mainBannerCircles/35 rounded-full absolute bottom-2 z-4 2xl:size-162 hidden md:block"></div>
+          <div className="size-152 bg-mainBannerCircles/35 rounded-full absolute bottom-4 left-14 z-4 2xl:size-162 hidden md:block"></div>
           <OffLable />
         </div>
       </div>
-      <div className="absolute top-0 left-0">
+      <div className="absolute top-0 left-12 hidden 2xl:block">
         <img src={louster} alt="Hanging lamp fixture" />
       </div>
-      <div className="bg-headerPink size-4 rounded-full absolute bottom-56 left-36 "></div>
+      <div className="absolute bg-headerPink size-4 rounded-full  bottom-56 left-46 hidden 2xl:block"></div>
     </section>
   );
 };
--- src/ui/OffLable.jsx
+++ src/ui/OffLable.jsx
@@ -2,7 +2,7 @@ import offBG from "../assets/home/mainBanner/offBG.svg";
 
 const OffLable = () => {
   return (
-    <div className="absolute flex items-center justify-center z-6 right-[-3.5rem]">
+    <div className="absolute hidden items-center justify-center z-6 right-[-5rem] 2xl:flex">
       <div className="relative">
         <img src={offBG} alt="off-background" className="z-6" />
         <span className="absolute top-8 left-8 font-josefin font-bold text-4xl text-white text-center">
